9a68fcf5dec1e142ae329e304194c473fc137153
Le Thanh Vinh <thanhvinh2400@gmail.com>
2025-01-06 15:34:47 +0700

    fix ui

---
+++ Clinic-Management-System/ClinicManagementSystem/Converters/DateTimeOffsetToDateForCanlendarConverters.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManagementSystem.Converters
+{
+    public class DateTimeOffsetToDateForCanlendarConverters:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is DateTimeOffset dateTimeOffset)
+            {
+                return new DateTimeOffset(dateTimeOffset.Date);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is DateTimeOffset date)
+            {
+                return new DateTimeOffset(date.Date);
+            }
+            return null;
+        }
+    }
+}
